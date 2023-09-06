448206bd53a6dd4e57224d886a52792852f2a486
windows <1945350177@qq.com>
2023-09-06 11:11:25 +0800

    修复downloadFileSync下载代码错误bug

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ function downloadFileSync(filesURL, fileNames, filePath, success, error) {
   // 遍历 URL 列表并下载文件
   filesURL.forEach((fileUrl, index) => {
     const fileName = fileNames[index] || index; // 可以根据需要自定义文件名
-    const filePath = `${filePath}/${fileName}`;
+    const completePath = `${filePath}/${fileName}`;
     // 发起 GET 请求并将文件保存到本地
     request(fileUrl)
-      .pipe(fs.createWriteStream(filePath))
-      .on("close", success)
-      .on("error", error);
+      .pipe(fs.createWriteStream(completePath))
+      .on("close", success || function () {})
+      .on("error", error || function () {});
   });
 }
 
